6c26f850c75a8f0dbf38507f73135bac1a776b09
Aditya <adityapratapaps020@gmail.com>
2026-01-27 01:44:44 +0530

    fixes next config again back to previous

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  output: "export", // 👈 Netlify ke liye important
-
   images: {
-    unoptimized: true, // 👈 static export ke liye
     remotePatterns: [
       {
         protocol: "https",
